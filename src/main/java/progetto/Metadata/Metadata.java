e0d605f9fa725e3bf63313dacc02e56af48c0e3b
fededc18 <53344251+fededc18@users.noreply.github.com>
2020-02-22 13:49:41 +0100

    ultime modifiche

--- src/main/java/progetto/Metadata/Metadata.java
+++ src/main/java/progetto/Metadata/Metadata.java
@@ -13,13 +13,14 @@ public class Metadata {
 	/*
 	 * Costruttore della classe
 	 */
+	
 	public Metadata() {
 	Field[] fields = Produz_Agricola.class.getDeclaredFields(); // estrae gli attributi dalla classe Produz_Agricola
     for (int i = 0; i < fields.length; i++) {
     	Map<String, String> map = new HashMap<>(); //creiamo una mappa con le coppie chiave-valore
     	//andiamo ad inserire le coppie
     	map.put("alias", fields[i].getName());
-        map.put("sourceField", fields[i].getName().toUpperCase());//Non mi convinve questa parte
+        map.put("sourceField", fields[i].getName().toUpperCase());
         map.put("type", fields[i].getType().getSimpleName());
         metadata.add(map);
     	}
